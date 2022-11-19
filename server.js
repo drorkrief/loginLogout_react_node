6bcb1c1a0bd1a32929845242f64e1ea41e131d5d
dror krief <dror1krief@gmail.com>
2022-11-19 22:35:03 +0200

    new features 1. express, 2. nodemon

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const bcrypt = require("bcrypt");
+const express = require("express");
+const app = express();
+const port = 3033;
+
+app.get("/", async (req, res) => {
+  const salt = await bcrypt.genSaltSync(10);
+  const hash = await bcrypt.hashSync("generic", salt);
+  console.log(salt, hash, "see above");
+  res.send("Hello World! "+ salt +" " + hash,);
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 // bcrypt.hash("generic", 5, function (err, hash) {
 //     console.log("1: ",hash);
 //     // TODO: Store the hash in your password DB
 //   });
-  
+
 // const myPlaintextPassword = "generic";
 // const hash = bcrypt.hashSync(myPlaintextPassword, 5);
 // console.log("2: ",hash);
 
-bcrypt.genSalt(10, function (err, salt) {
-    console.log("salt : : : : ",salt); // the random salt string
-    bcrypt.hash("generic", salt, function (err, hash) {
-        console.log("hash + salt: ",hash);
-        // TODO: Store the hash in your password DB
-      });
+// bcrypt.genSalt(10, function (err, salt) {
+//     console.log("salt : : : : ",salt); // the random salt string
+//     bcrypt.hash("generic", salt, function (err, hash) {
+//         console.log("hash + salt: ",hash);
+//         // TODO: Store the hash in your password DB
+//       });
 
-  });
+//   });
+
+//   do it in only two lines:
